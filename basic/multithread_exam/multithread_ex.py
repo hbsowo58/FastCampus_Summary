0859d16892b66aea2fbb54ed9070c49ea99f7196
hbsowo58 <hbsowo58@gmail.com>
2019-04-23 15:05:18 +0900

    add multithread_exam.py

---
+++ basic/multithread_exam/multithread_ex.py
@@ -0,0 +1,25 @@
+import threading
+
+def thread_main(li, n):
+	for i in range(offset * n, offset *(n + 1)):
+		li[i] *= 2
+
+n = 1000 
+
+offset = n // 4
+
+li = [i+1 for i in range(n)]
+threads = []
+
+for i in range(4):
+	th = threading.Thread(target = thread_main,
+                          args = (li, i))
+	threads.append(th)
+
+for th in threads:
+	th.start()
+
+for th in threads:
+	th.join()
+
+print(li)
---
+++ basic/multithread_exam/multithread_exam.py
@@ -0,0 +1,29 @@
+import threading
+
+n=1000
+offset=n//4
+
+def thread_main(li, i):
+    for idx in range(offset*i, offset*(i+1)):
+        li[idx]*=2
+
+
+li = [i for i in range(1,1001)]
+threads=[]
+
+# 스레드를 생성
+for i in range(4):
+    th=threading.Thread(
+        target=thread_main, args=(li, i))
+    threads.append(th)
+
+# 멀티 스레딩
+for th in threads:
+    th.start()
+
+# 메인 스레드에서 나머지 스레드들이 
+# 모든 실행을 끝날 때까지 기다린다
+for th in threads:
+	th.join()
+
+print(li)
